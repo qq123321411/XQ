1a236552b31eb66ac9133f8fd5295f4c05a0be13
qq123321411 <burou111@outlook.com>
2026-08-19 08:34:51 +0800

    Update config.js

--- dating-site/js/config.js
+++ dating-site/js/config.js
@@ -1,7 +1,7 @@
 // 在这里填写 Supabase 项目信息。
 // 只填写 Project URL + anon/publishable key；绝不要放 service_role/secret key。
-window.SUPABASE_URL = 'YOUR_SUPABASE_URL';
-window.SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_OR_PUBLISHABLE_KEY';
+window.SUPABASE_URL = 'https://yutraouosffjteivvlfx.supabase.co/rest/v1/';
+window.SUPABASE_ANON_KEY = 'sb_publishable_9kM95KpOX6WPwcXowvPbWQ_s0_YJUEA';
 
 window.supabaseClient = null;
 if (window.supabase && !window.SUPABASE_URL.startsWith('YOUR_') && !window.SUPABASE_ANON_KEY.startsWith('YOUR_')) {
